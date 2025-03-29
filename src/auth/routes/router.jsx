9fe7ebd6324c91c83fd4cf8e7104e3e377fe885b
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-29 08:58:35 +0600

    first commit

---
+++ src/auth/routes/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This home page</div>,
+  },
+])
+
+export default router;
+
